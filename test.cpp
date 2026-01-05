ce4b3e0271ba4c63725cb2b21bf46953ba164048
aruvins <aidan.ruvins@yahoo.com>
2026-01-05 10:57:59 -0500

    Test code

--- test.cpp
+++ test.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 
-
 int main() {
     // Solution comes here
-    int x;
-    while (cin >> x){
-        cout << x << "\n";
-    }
+    int a = 123456789;
+    long long b = a * a;
+    cout << b << "\n";
+
+    return 0;
 }
